9af6ca79b2b64572a017c358c31f432a218bf3a3
My95dn <doanvanmy.1995@gmail.com>
2022-12-12 22:02:28 +0700

    demo

---
+++ todo/hoc.js
@@ -0,0 +1,20 @@
+const tabItem = document.querySelectorAll('.tab-item')
+const tabPane = document.querySelectorAll('.tab-pane')
+const sub = document.querySelector('.line')
+        
+tabItem.forEach((tab, index)=> {
+    tab.onclick = function() {
+        sub.style.left = this.offsetLeft + 'px'
+        sub.style.width = this.offsetWidth + 'px'
+        
+        let good = tabPane[index]
+        const active = document.querySelector('.tab-item.active')
+        const delay = document.querySelector('.tab-pane.active')
+        active.classList.remove('active')
+        delay.classList.remove('active')
+        this.classList.add('active')
+        good.classList.add('active')
+        
+    }
+}) 
+    
--- todo/main.js
+++ todo/main.js
@@ -2,28 +2,84 @@ var input = document.querySelector('#father');
 var summit = document.getElementsByTagName("button")[0]; 
 var lis = document.getElementsByTagName("ul")[0];
 var subtext = document.getElementsByTagName("li")[0];
+var know = document.querySelector('.return')
+var up = document.querySelector('delate')
+
+RenderElement(localstorage())
 summit.onclick = function() {
-   if(!input) {
+   if(!input.value) {
       alert('vui lòng nhập thông tin')
       return false
+      
    }
-   var value = input.value
-   var array = []
-   array.push({
-      name: value
-   })
+   
+   
+   var array = localstorage()
+   let buttons = this.getAttribute('id')
+   let tank = {name: input.value}
+   if(buttons === 0 || buttons) {
+     
+      array[buttons] = tank
+      this.removeAttribute('id')
+      
+   } else {
+      array.push(tank)
+   }
+
+   
+   
+   
+      
+   
    input.value = ''
-   handlElement(array)
+   localStorage.setItem('array', JSON.stringify(array))
+   
+   RenderElement(array)
+   
 
 }
-function handlElement(array = []) {
+
+function delate(id) {
+   if(confirm('bạn có thât sự muốn xóa')) {
+      var delate = localstorage()
+      delate.splice(id, 1)
+      localStorage.setItem('array', JSON.stringify(delate))
+      RenderElement(delate)
+      
+   }
+   
+}
+function update(id) {
+   let good = localstorage()
+   if(good.length > 0) {
+      input.value = good[id].name
+      summit.setAttribute('id', id)
+     
+   }
+}
+function RenderElement(element) {
    var sub = '<ul>'
-   array.forEach((e)=> {
-      sub += `<li>${e.name}</li>`
+   element.forEach((e, index)=> {
+      sub += `<li>${e.name}
+      <span class="delate" onclick ="delate(${index})">xóa</span>
+      <span class="upload" onclick ="update(${index})">sửa</span>
+      </li>
+               
+               
+      `  
    }) 
    sub += '</ul>'
-   document.querySelector('.return').innerHTML = sub
+   know.style.color = 'aqua'
+   know.style.backgroundColor = '#333'
+   know.style.margin = '30px'
+    
+   know.innerHTML = sub
 
  
    
 }
+function localstorage() {
+   return localStorage.getItem('array') ? JSON.parse(localStorage.getItem('array')) : []
+}
+
+
